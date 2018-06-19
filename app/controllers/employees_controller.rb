6f7b0f1953281d79a9b85a15328e4f9ddba04d5d
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-06-19 10:54:33 -0400

    added employee forms

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,25 +4,31 @@ class EmployeesController < ApplicationController
     @employees = Employee.all
   end
 
-  def new
-    @employee = Employee.new
-  end
-
   def show
     find_employee
   end
 
+  def new
+    @employee = Employee.new
+    @all_managers = Manager.all
+  end
+
   def create
     @employee = Employee.create(employee_params)
+    @all_managers = Manager.all
+    redirect_to @employee
   end
 
   def edit
     find_employee
+    @all_managers = Manager.all
   end
 
   def update
     @employee = Employee.find(params[:id])
     @employee.update(employee_params)
+    @all_managers = Manager.all
+    redirect_to @employee
   end
 
   def destroy
@@ -36,7 +42,7 @@ class EmployeesController < ApplicationController
     end
 
     def employee_params
-      params.require(:employee).permit(:name, :title)
+      params.require(:employee).permit(:name, :title, :manager_id)
     end
 
 end
