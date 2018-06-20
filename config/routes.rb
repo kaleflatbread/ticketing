c34b594680f211a3fadf09a7a455ca6d2c49d93d
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-06-20 10:34:16 -0400

    built helpers, view and model for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :projects
   resources :employee_projects
   resources :employees
+  resources :sessions, only: [:new, :create]
+  delete "/sessions", to: "sessions#destroy", as: "destroy_session"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
